3d09edbb071fdf79aeba81cc927e66465e5ae381
Margarida <margarida.martins@ua.pt>
2022-10-10 16:12:38 +0100

    simple billing

--- api/src/main.py
+++ api/src/main.py
@@ -1,6 +1,10 @@
+from datetime import date
+from fcntl import DN_DELETE
 import logging
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from typing import List
+
 from utils import create_response
 
 from simple import simple
@@ -13,8 +17,43 @@ logging.basicConfig(
 
 app = FastAPI()
 
-@app.get("/api/v1/simple")
-def simple_request()-> JSONResponse:
+@app.get("/api/v1/optimize")
+def simple_request(
+        price: float,
+        low_price: float = 0,
+        high_price: float = 0,
+        type: str = "S",
+        start: int = 0,
+        end: int = 744
+    )-> JSONResponse:
+    """
+    Endpoint ``/optimize`` that accepts the method GET. Returns the best confort score / price ratio.
+    Parameters:
+        type : `str`
+            The type of the energy pricing, can be S (simple), B (bi-hour) or T (tri-hour)
+        price : `List[float]`
+            The energy billing price is a list, with length 1 for S type, 2 for B and 3 for T.
+        start : `int`
+            index of first record to be considered (inclusive)
+        end : `int`
+            index of last record to be considered (exclusive)
+    Returns
+    -------
+        response : `JSONResponse`
+            Json response with the status code and data.
+    """
+    try:
+        if type == "S":
+            return create_response(status_code=200, data=simple(price, start, end))
+        elif type == "B":
+            #TODO
+            pass
+        elif type == "T":
+            pass
+            #TODO
+        return create_response(status_code=400, message="Wrong billing type")
+    except BaseException as e:
+        logging.debug(e)
+        return create_response(status_code=400, message="Bad arguments")
     
-    return create_response(status_code=200, data=simple(1))
 
--- api/src/simple.py
+++ api/src/simple.py
@@ -17,26 +17,26 @@ def simple(price, start=0, end=744):
         if t<0:
             c_score += 4
             kw += 1.6
-            total_price += price * kw
+            total_price += price * 1.6
             data.append({'time':time, 'mode': 'ECO', 'c_score': 4, 'kwh': 1.6, 'total_c_score': c_score, 'total_kwh': kw, 'temp': t, 'total_cost':total_price})
             
 
         elif t<10:
             c_score += 8
             kw += 2.4
-            total_price += price * kw
+            total_price += price * 2.4
             data.append({'time':time, 'mode': 'COMFORT', 'c_score': 8, 'kwh': 2.4, 'total_c_score': c_score, 'total_kwh': kw, 'temp': t, 'total_cost':total_price})
         
         elif t<20:
             c_score += 8
             kw += 1.6
-            total_price += price * kw
+            total_price += price * 1.6
             data.append({'time':time, 'mode': 'COMFORT', 'c_score': 8, 'kwh': 1.6, 'total_c_score': c_score, 'total_kwh': kw, 'temp': t, 'total_cost':total_price})
         
         else:
             c_score += 8
             kw += 0.8
-            total_price += price * kw
+            total_price += price * 0.8 
             data.append({'time':time, 'mode': 'COMFORT', 'c_score': 8, 'kwh': 0.8, 'total_c_score': c_score, 'total_kwh': kw, 'temp': t, 'total_cost':total_price})
 
     return data
