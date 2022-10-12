f7ff2f47e7a41c438cd0416878d8c224d753bf3d
Margarida <margarida.martins@ua.pt>
2022-10-12 16:32:53 +0100

    deadhours documented and finished

--- api/src/algorithms.py
+++ api/src/algorithms.py
@@ -133,4 +133,94 @@ def min_cost(data_path, prices, start, end):
     return data
 
 def dead_hours(data_path, prices, start, end, dead_hours):
-    pass
+    """
+    Algorithm to calculate results with best score/price ratio.
+    Parameters
+    ----------
+        data_path : `str`
+            Path to the file with measurements.
+
+        prices : `list with float values`
+            List with all the prices.
+
+        start : `DateTime`
+            First day of the search interval.
+
+        end : `DateTime`
+            Last day of the search interval.
+
+        dead_hours: `List[int]`
+            List with the hours where heating does not matter
+
+    Returns
+    -------
+        response : `list with dictionaries`
+            List with dictionaries, where each dictionary is a group of statistics related to an hour.
+    """
+    all_temps = get_data(data_path, start, end)
+
+    data = []
+
+
+    # iterate through day-by-day measurements
+    for temps in all_temps:
+        
+        extra_cost = []
+        for i, t in enumerate(temps):
+            if int(t[1][11:13]) not in dead_hours: # check if it is not a dead hour
+                if t[0]<0:
+                    extra_cost.append((9.8*prices[i], i))
+
+                elif t[0]<10:
+                    extra_cost.append((0.8*prices[i], i))
+                
+                elif t[0]<20:
+                    extra_cost.append((0.8*prices[i], i))
+                
+                else:
+                    extra_cost.append((0.4*prices[i], i))
+            else: # dead hours are always ECO
+                if t[0] < 0:
+                    data.append({'time':t[1], 'mode': 'ECO', 'c_score': 4, 'kwh': 1.6, 'cost': 1.6*prices[i], 'temperature': t[0]})
+                
+                elif t[0]<20:
+                    data.append({'time':t[1], 'mode': 'ECO', 'c_score': 4, 'kwh': 0.8, 'cost': 0.8*prices[i], 'temperature': t[0]})
+                    
+                else:
+                    data.append({'time':t[1], 'mode': 'ECO', 'c_score': 4, 'kwh': 0.4, 'cost': 0.4*prices[i], 'temperature': t[0]})
+
+
+
+        extra_cost = sorted(extra_cost, key = lambda x: x[0])
+
+        indexes = [x[1] for x in extra_cost[0:7]]
+
+        # iterate through measurements of a day
+        for i, t in enumerate(temps):
+            price = prices[i]
+
+            if int(t[1][11:13])  not in dead_hours:
+                if i in indexes:
+                    if t[0]<0:
+                        data.append({'time':t[1], 'mode': 'COMFORT', 'c_score': 8, 'kwh': 1.6, 'cost': 11.4*price, 'temperature': t[0]})
+
+                    elif t[0]<10:
+                        data.append({'time':t[1], 'mode': 'COMFORT', 'c_score': 8, 'kwh': 2.4, 'cost': 2.4*price, 'temperature': t[0]})
+                    
+                    elif t[0]<20:
+                        data.append({'time':t[1], 'mode': 'COMFORT', 'c_score': 8, 'kwh': 1.6, 'cost': 1.6*price, 'temperature': t[0]})
+                    
+                    else:
+                        data.append({'time':t[1], 'mode': 'COMFORT', 'c_score': 8, 'kwh': 0.8, 'cost': 0.8*price, 'temperature': t[0]})
+                
+                else:
+                    if t[0]<10:
+                        data.append({'time':t[1], 'mode': 'ECO', 'c_score': 4, 'kwh': 1.6, 'cost': 1.6*price, 'temperature': t[0]})
+                    
+                    elif t[0]<20:
+                        data.append({'time':t[1], 'mode': 'ECO', 'c_score': 4, 'kwh': 0.8, 'cost': 0.8*price, 'temperature': t[0]})
+                    
+                    else:
+                        data.append({'time':t[1], 'mode': 'ECO', 'c_score': 4, 'kwh': 0.4, 'cost': 0.4*price, 'temperature': t[0]})
+
+    return data
--- api/src/main.py
+++ api/src/main.py
@@ -2,6 +2,7 @@ import logging
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from typing import List
+from fastapi import Query
 import datetime
 import os
 
@@ -33,7 +34,7 @@ def simple_request(
         tariff: str = "S",
     )-> JSONResponse:
     """
-    Endpoint ``/optimize`` that accepts the method GET. Returns the best confort score / price ratio.
+    Endpoint ``/optimize`` that accepts the method GET. Returns the best price for a minimum of 124 confort score.
     Parameters:
         start: `str`
             The start datetime as string (inclusive)
@@ -61,15 +62,51 @@ def simple_request(
         logging.debug(e)
         print(e)
         return create_response(status_code=400, message="Bad arguments")
-    
+
+@app.get("/api/v1/bestratio")
+def simple_request(
+        start:  datetime.datetime,
+        end: datetime.datetime,
+        supplier: str,
+        tariff: str = "S",
+    )-> JSONResponse:
+    """
+    Endpoint ``/bestratio`` that accepts the method GET. Returns the best confort score / price ratio.
+    Parameters:
+        start: `str`
+            The start datetime as string (inclusive)
+        end: `str`
+            The end datetime as string (exclusive)
+        supplier:
+            The name of the supplier
+        tariff: `str`
+            The corresponding tariff type. Available "S", "B", "T"
+    Returns
+    -------
+        response : `JSONResponse`
+            Json response with the status code and data.
+    """
+    try:
+        temp_data = data_path + 'temperatureData.json'
+        prices = get_prices(supplier, tariff)
+
+        if len(prices) == 0:
+            return create_response(status_code=400, message="Wrong billing type")
+        
+        return create_response(status_code=200, data=best_ratio(temp_data, prices, start, end))
+        
+    except BaseException as e:
+        logging.debug(e)
+        print(e)
+        return create_response(status_code=400, message="Bad arguments")
 
 @app.get("/api/v1/deadhours")
 def deadhours_request(
         start: datetime.datetime,
         end: datetime.datetime,
-        hours: List[int],
         supplier: str,
-        tariff: str = "S"
+        tariff: str = "S",
+        hours: List[int] = Query(...)
     ) -> JSONResponse:
     """
     Endpoint ``/deadhours`` that accepts the method GET. Returns the best price for a minimum confort score of 124 considering dead hours,
@@ -97,9 +134,12 @@ def deadhours_request(
         if len(prices) == 0:
             return create_response(status_code=400, message="Wrong billing type")
         
+
         return create_response(status_code=200, data=dead_hours(temp_data, prices, start, end, hours))
         
     except BaseException as e:
         logging.debug(e)
         print(e)
         return create_response(status_code=400, message="Bad arguments")
+
+
