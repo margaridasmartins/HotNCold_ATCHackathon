76f96071a8ebda4d5cfeaae53c9b1a0d964fa386
brunosbastos <brunosb@ua.pt>
2022-10-13 22:37:02 +0100

    update api service

--- frontend/src/services/ApiService.js
+++ frontend/src/services/ApiService.js
@@ -15,6 +15,27 @@ class ApiService {
         // TODO: hardcoded
         return this.get_data(location, "2021-12-01T00:00:00", "2021-12-02T00:00:00", supplier, tariff);
     }
+
+    get_last_month(location, supplier, tariff) {
+        let curr_date = new Date();
+        const end = convertDateToFormat(curr_date);
+        curr_date.setMonth(curr_date.getMonth() - 1);
+        const start = convertDateToFormat(curr_date);
+
+        // TODO: hardcoded
+        return this.get_data(location, "2021-12-01T00:00:00", "2021-12-02T00:00:00", supplier, tariff);
+    }
+
+    get_last_week(location, supplier, tariff) {
+        let curr_date = new Date();
+        const end = convertDateToFormat(curr_date);
+        curr_date.setDate(curr_date.getDate() - 7);
+        const start = convertDateToFormat(curr_date);
+
+        // TODO: hardcoded
+        return this.get_data(location, "2021-12-01T00:00:00", "2021-12-02T00:00:00", supplier, tariff);        
+    }
+
 }
 
 function convertDateToFormat(d) {
